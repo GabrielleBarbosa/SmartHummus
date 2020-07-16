9cc8b699c30fd803ad0e080f9799ab4d26792f03
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2020-07-16 19:31:19 -0300

    criação de telas de login e cadastro

---
+++ app/smarthummus_app/lib/screens/sign_up_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(),
+          Row(
+            children: [],
+          ),
+          Row(),
+          Container()
+        ],
+      ),
+    );
+  }
+}
